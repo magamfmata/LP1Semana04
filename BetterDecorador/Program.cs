c6a51ad0ea58b8be16c6539cb62392330442a068
Magali <magamayrmata@gmail.com>
2025-03-13 23:17:24 +0000

    ex 1 done

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 
-namespace BetterDecorador
-{
+    namespace BetterDecorador
+    {
+
+    }
+
     public class Program
     {
+        private static string Decor(string s, char dec, int count)
+        {
+            return $"{dec}{dec}{dec} {s} {dec}{dec}{dec}";
+        }
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            if (args.Length==3);
+            {
+                string s = args[0];
+                char c = char.Parse(args[1]);
+                Console.WriteLine(Decor(s,c,int.Parse(args[2])));
+            }
         }
+
+
     }
-}
