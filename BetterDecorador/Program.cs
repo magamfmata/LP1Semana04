f1dc80a81d4ce574740c653185b78e5bbb7a5373
Magali <magamayrmata@gmail.com>
2025-03-14 00:21:14 +0000

    comments

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -7,10 +7,25 @@
 
     public class Program
     {
+        /// <summary>
+        /// decorates a string 
+        /// takes 3 arguments and converts them into a string
+        /// multiplies the char
+        /// </summary>
+        /// <param name="s">string that is being decorated</param>
+        /// <param name="dec">char that is decorating the string</param>
+        /// <param name="count">multiplies the char</param>
+        /// <returns>string that is decorated</returns>
         private static string Decor(string s, char dec, int count)
         {
             return $"{dec}{dec}{dec} {s} {dec}{dec}{dec}";
         }
+        /// <summary>
+        /// method that receives parameters and shows decorated string
+        /// checks if the correct argument numbers was made
+        /// converts it to the desired type
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
             if (args.Length==3);
