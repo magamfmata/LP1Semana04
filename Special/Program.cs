3dcdcde607e537b7d1a593c4984e5628f543bca2
Magali <magamayrmata@gmail.com>
2025-03-14 00:52:12 +0000

    ex 4

--- Special/Program.cs
+++ Special/Program.cs
@@ -10,7 +10,9 @@ namespace Special
           {
             return 0;
           }
+
           else if (n==1)
+          
           {
             return 1;
           }
@@ -18,6 +20,13 @@ namespace Special
           {
             return GetSpecial(n - 1) + 2 * GetSpecial(n - 2);
           }
-          }
+        
+        }
+        private static void Main(string[] args)
+        {
+            Console.Write("Insert n-th number of the special sequence: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetSpecial(n));
+        }
         }
     }
