9c24f46084965a2e9075ff8f7cfc1e667e9bc0f1
Magali <magamayrmata@gmail.com>
2025-03-14 00:54:13 +0000

    Final commit

--- Special/Program.cs
+++ Special/Program.cs
@@ -6,20 +6,20 @@ namespace Special
     {
         private static int GetSpecial(int n)
         {
-          if (n==0);
-          {
+          if (n==0)
+        {
             return 0;
-          }
+        }
 
           else if (n==1)
-          
-          {
+
+        {
             return 1;
-          }
+        }
             else
-          {
+        {
             return GetSpecial(n - 1) + 2 * GetSpecial(n - 2);
-          }
+        }
         
         }
         private static void Main(string[] args)
